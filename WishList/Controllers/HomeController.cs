f0e9735cd47db275ca51ab63a45799f76f4fb13f
Амиржан Толебай <amirzhan.tolebay@gmail.com>
2020-10-10 10:46:05 +0600

    Practice Tasks

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
